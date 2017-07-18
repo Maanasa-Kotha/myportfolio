ddeed53fb58c9de6af1872d5789280a0dd363260
fantasmical <maanasa.kotha@gmail.com>
2017-07-18 09:34:04 -0700

    Update slides.js

--- slides.js
+++ slides.js
@@ -1,27 +1,26 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides(n){
+function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlides(n){
+function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n){
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dots");
-  if(n>slides.length) {slideIndex = 1}
-  if(n>1) {slideIndex = slides.length}
-  for(n<1) {slideIndex = slides.length}
-  for (i=0, i<slides.length; i++){
-    slides[i].style.display = "none";
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  for(i=0;i<dots.length;i++){
-    dots[i].className = dots[i].className.replace("active", "");
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"
-  dots[slideIndex-1].classNmae += "active";
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
